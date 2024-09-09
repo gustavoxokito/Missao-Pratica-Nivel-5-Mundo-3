cebfd40df8768ee71f727a82174f34b6baa85d0e
gustavoxokito <gustavo.xokito@gmail.com>
2024-09-09 19:40:43 -0300

    first commit

---
+++ CadastroServer/src/cadastroserver/controller/exceptions/NonexistentEntityException.java
@@ -0,0 +1,13 @@
+/**
+ * @author Cleyton
+ */
+package cadastroserver.controller.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ CadastroServer/src/cadastroserver/controller/exceptions/PreexistingEntityException.java
@@ -0,0 +1,13 @@
+/**
+ * @author Cleyton
+ */
+package cadastroserver.controller.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
